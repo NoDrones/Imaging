d6277aab7ca242c7b065412bed09a249ae491562
Calvin Ryan <calvinryan@Calvins-MacBook-Pro.local>
2018-02-03 18:12:42 -0500

    improved leaf detection across lighting conditions. I think...

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import sensor, image, time, pyb
 
-sensor.reset()                      # Reset and initialize the sensor.
-print("just reset")
-sensor.set_gainceiling(16)
-sensor.set_pixformat(sensor.RGB565) # Set pixel format to RGB565
-sensor.set_framesize(sensor.QVGA)   # Set frame size to QVGA (320x240)
-                                    # Can't do operations on RGB565 @ VGA resolution because not enough RAM. Image is automatically and forcibly compressed to JPEG.
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.skip_frames(time = 2000)
+sensor.set_auto_gain(False) # must be turned off for color tracking
+sensor.set_auto_whitebal(False) # must be turned off for color tracking
+clock = time.clock()
 
-sensor.set_auto_whitebal(False)
-
-print("just disabled exp and white")
-sensor.skip_frames(n = 60)          # Wait for settings take effect.
-print("just skipped frames")
 
 '''
 #get white balance. You have two seconds.
@@ -38,42 +34,42 @@ t_elapsed = 0
 for i in range(1000):
     img = sensor.snapshot()         # Take a picture and return the image.
 
-    #img.histeq()
-
     '''
     L = Lightness where 0 is black and 100 is white
-    A = position between red and green where -127 is green and 128 is red
-    B = position between yellow and blue where -127 is blue and 128 is yellow.
+    A = -127 is green and 128 is red
+    B = -127 is blue and 128 is yellow.
     '''
 
-    stage_one_thresholds = [(0, 100, -127, 0, 0, 60)] #LAB -> [Llo, Lhi, Alo, Ahi, Blo, Bhi]
-    stage_two_thresholds = [(10, 50, -10, 40, -30, 30)]
+    #Discriminate against median to determine lighting conditions
+    #3 bins:
+    #median < 20 = dimly lit (18 maybe?)
+    #20 =< median =< 40 = well lit
+    #median > 40 = over lit
 
+    #thresholds LAB -> [Llo, Lhi, Alo, Ahi, Blo, Bhi]
+    #stage_one_thresholds = [(0, 100, -127, -10, 0, 60)]
 
-    for blob_index, stage_one_blob in enumerate(img.find_blobs(stage_one_thresholds, pixels_threshold=50, area_threshold=50, merge = False)):
+    img_hist = img.get_histogram()
+    img_stats = img_hist.get_statistics()
 
-        cur_hist = img.get_histogram(roi = (stage_one_blob[0], stage_one_blob[1], stage_one_blob[2], stage_one_blob[3]))
-        cur_stats = cur_hist.get_statistics()
+    stage_one_thresholds = [(0, 100, -127, img_stats.a_mode() - 12, img_stats.b_mode() + 12, 60)]
 
-        #Use threshold_cb for this functionality!!!
-        if abs(cur_stats.a_mean() - cur_stats.b_mean()) > 20: #if the a and b histograms are close together it means the color is probably white and should be discarded
+    for blob_index, stage_one_blob in enumerate(img.find_blobs(stage_one_thresholds, pixels_threshold=100, area_threshold=100, merge = True)):
+        blob_hist = img.get_histogram(roi = (stage_one_blob[0], stage_one_blob[1], stage_one_blob[2], stage_one_blob[3]))
+        blob_stats = blob_hist.get_statistics()
+
+        if (abs(blob_stats.a_mean() - blob_stats.b_mean()) > 25) & (blob_stats.a_max() - blob_stats.a_min() > 20) & (blob_stats.b_max() - blob_stats.b_min() > 20): #if the a and b histograms are close together it means the color is probably white and should be discarded
+
             img.draw_rectangle(stage_one_blob.rect(), color = 120)
 
-            l_blue.on()
-            time.sleep(50)
-            l_blue.off()
-            img.save("/stg_one_blob_" + str(blob_index), 100, (stage_one_blob[0], stage_one_blob[1], stage_one_blob[2], stage_one_blob[3]))
+            l_blue.toggle()
 
+            stage_two_thresholds = [(0, blob_stats.l_mode() + 5, blob_stats.a_mean(), 128, -127, blob_stats.b_mean())]
 
             for stage_two_blob in img.find_blobs(stage_two_thresholds, merge = False, roi = (stage_one_blob[0], stage_one_blob[1], stage_one_blob[2], stage_one_blob[3])):
                 img.draw_rectangle(stage_two_blob.rect(), color = 0)
 
-                #img.save("/blob" + str(blob_index), 100, roi = (stage_one_blob[0], stage_one_blob[1], stage_one_blob[2], stage_one_blob[3]))
+        img.save("/blob_" + str(blob_index), 100, (stage_one_blob[0], stage_one_blob[1], stage_one_blob[2], stage_one_blob[3]))
 
-
-
-#need these to flush the current image (if only taking a single image) in the frame buffer to the IDE and then give time for the blob containers to appear
 sensor.flush()
-time.sleep(1000)
-
-
+time.sleep(3000)
