437675d242dc9f3633bbd4a4db7920532c07c5c9
NHerbert141 <NHerbert141@users.noreply.github.com>
2018-02-04 15:17:58 -0500

    Differentiate me from Calvin

--- NicksCamera/main.py
+++ NicksCamera/main.py
@@ -38,8 +38,6 @@ t_elapsed = 0
 for i in range(1000):
     img = sensor.snapshot()         # Take a picture and return the image.
 
-    #img.histeq()
-
     '''
     L = Lightness where 0 is black and 100 is white
     A = position between red and green where -127 is green and 128 is red
@@ -49,28 +47,11 @@ for i in range(1000):
     stage_one_thresholds = [(0, 100, -127, 0, 0, 60)] #LAB -> [Llo, Lhi, Alo, Ahi, Blo, Bhi]
     stage_two_thresholds = [(10, 50, -10, 40, -30, 30)]
 
-
     for blob_index, stage_one_blob in enumerate(img.find_blobs(stage_one_thresholds, pixels_threshold=50, area_threshold=50, merge = False)):
-
-        cur_hist = img.get_histogram(roi = (stage_one_blob[0], stage_one_blob[1], stage_one_blob[2], stage_one_blob[3]))
-        cur_stats = cur_hist.get_statistics()
-
-        #Use threshold_cb for this functionality!!!
-        if abs(cur_stats.a_mean() - cur_stats.b_mean()) > 20: #if the a and b histograms are close together it means the color is probably white and should be discarded
-            img.draw_rectangle(stage_one_blob.rect(), color = 120)
-
-            l_blue.on()
-            time.sleep(50)
-            l_blue.off()
-            img.save("/stg_one_blob_" + str(blob_index), 100, (stage_one_blob[0], stage_one_blob[1], stage_one_blob[2], stage_one_blob[3]))
-
-
-            for stage_two_blob in img.find_blobs(stage_two_thresholds, merge = False, roi = (stage_one_blob[0], stage_one_blob[1], stage_one_blob[2], stage_one_blob[3])):
-                img.draw_rectangle(stage_two_blob.rect(), color = 0)
-
-                #img.save("/blob" + str(blob_index), 100, roi = (stage_one_blob[0], stage_one_blob[1], stage_one_blob[2], stage_one_blob[3]))
-
-
+        centroid_x = stage_one_blob.cx()
+        centroid_y = stage_one_blob.cy()
+        pixel = img.get_pixel(centroid_x, centroid_y)
+        printf(pixel)
 
 #need these to flush the current image (if only taking a single image) in the frame buffer to the IDE and then give time for the blob containers to appear
 sensor.flush()
