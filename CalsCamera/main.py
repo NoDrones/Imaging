aeb3aa619fcff3105356b0a3f1e0d91193edfb2f
Calvin Ryan <calvinryan@Calvins-MacBook-Pro.local>
2018-03-01 13:53:11 -0500

    some auto exposure stuff. Also added nick's i2c send code in order to send calibration vals

--- CalsCamera/main.py
+++ CalsCamera/main.py
@@ -1,15 +1,15 @@
 #Author: Calvin Ryan
-import sensor, image, time
+import sensor, image, time, pyb, ustruct
 
-def set_custom_exposure(high_l_mean_thresh = 14, low_l_mean_thresh = 13):
+def set_custom_exposure(high_l_mean_thresh = 17, low_l_mean_thresh = 16):
     try:
         print("Starting Exposure Adjustment...")
         b_gain = sensor.__read_reg(0x01)
         r_gain = sensor.__read_reg(0x02)
         g_gain = sensor.__read_reg(0x03)
-        r_gain = round(r_gain/3)
-        g_gain = round(g_gain/3)
-        b_gain = round(b_gain/3)
+        r_gain = round(r_gain/6)
+        g_gain = round(g_gain/6)
+        b_gain = round(b_gain/6)
         sensor.__write_reg(0x01, b_gain)
         sensor.__write_reg(0x02, r_gain)
         sensor.__write_reg(0x03, g_gain)
@@ -18,36 +18,46 @@ def set_custom_exposure(high_l_mean_thresh = 14, low_l_mean_thresh = 13):
         img_stats = img.get_statistics()
         l_mean = img_stats.l_mean()
         count = 0
-        cur_gain = sensor.__read_reg(0x00)
+        print("7: " + str(sensor.__read_reg(0x00) & 0b10000000))
+        ceiling = (sensor.__read_reg(0x00) & 0b01110000) #0b00010000
+        cur_gain = sensor.__read_reg(0x00) & 0b00001111
 
-        while(((l_mean > high_l_mean_thresh) | (l_mean < low_l_mean_thresh))) & (count < 1000):
+        while(((l_mean > high_l_mean_thresh) | (l_mean < low_l_mean_thresh))) & (count < 256) & (cur_gain >= 0):
 
             img = sensor.snapshot()         # Take a picture and return the image.
             img_stats = img.get_statistics()
             l_mean = img_stats.l_mean()
 
-            #print("current gain: " + str(cur_gain))
-            #print("l_mean: " + str(l_mean))
+            print("gain_cieling: " + str(ceiling))
+            print("current gain: " + str(cur_gain))
+            print("l_mean: " + str(l_mean))
 
             if l_mean > high_l_mean_thresh:
-                new_gain = cur_gain - 1
+                if cur_gain == 0:
+                    ceiling -= 1
+                new_gain = (cur_gain - 1) & 0b00001111
+                send = new_gain | ceiling
             elif l_mean < low_l_mean_thresh:
-                new_gain = cur_gain + 1
+                if cur_gain == 15:
+                    ceiling += 1
+                new_gain = (cur_gain + 1) & 0b00001111
+                send = new_gain | ceiling
             else:
                 break #we're in the range now!
 
-            sensor.__write_reg(0x00, new_gain)
+            sensor.__write_reg(0x00, send)
             cur_gain = new_gain
             count += 1
 
-        if count < 1000:
+        if (count < 256) | (cur_gain == 0):
             print("Exposure Adjustment Complete.")
             return l_mean
         else:
             print("Exposure Adjustment Incomplete.")
             return -1
 
-    except:
+    except Exception as e:
+        print(e)
         print("Error occured!")
         return -2
 
@@ -55,12 +65,15 @@ def set_custom_exposure(high_l_mean_thresh = 14, low_l_mean_thresh = 13):
 if __name__ == "__main__":
 
     ########### SETUP STUFF
-
     sensor.reset()
     sensor.set_pixformat(sensor.RGB565)
     sensor.set_framesize(sensor.QVGA)
     sensor.skip_frames(time = 2000)
     clock = time.clock()
+
+    i2c_obj = pyb.I2C(2, pyb.I2C.SLAVE, addr=0x12)
+    i2c_obj.deinit() # Fully reset I2C device...
+    i2c_obj = pyb.I2C(2, pyb.I2C.SLAVE, addr=0x12)
 
     #get in focus balance. You have two seconds.
     t_start = time.ticks()
@@ -69,11 +82,62 @@ if __name__ == "__main__":
         img = sensor.snapshot()
         t_elapsed = time.ticks() - t_start
 
-    sensor.set_auto_gain(False) # must be turned off for color tracking
+    print("before: " + str(sensor.__read_reg(0x00)))
+    print("7: " + str(sensor.__read_reg(0x00) & 0b10000000))
+
+    sensor.set_auto_gain(True) # must be turned off for color tracking
     sensor.set_auto_whitebal(False) # must be turned off for color tracking
     sensor.set_auto_exposure(False)
-    set_l_mean = set_custom_exposure() #default thresholds
     sensor.set_contrast(+3)
+
+    print("after: " + str(sensor.__read_reg(0x00)))
+    print("7: " + str(sensor.__read_reg(0x00) & 0b10000000))
+
+    print()
+    pre_adjust_r_gain = sensor.__read_reg(0x02)
+    pre_adjust_g_gain = sensor.__read_reg(0x03)
+    pre_adjust_b_gain = sensor.__read_reg(0x01)
+    pre_adjust_overall_gain = sensor.__read_reg(0x00)
+    pre_adjust_exposure = (sensor.__read_reg(0x08) << 8) + sensor.__read_reg(0x10)
+
+    print("R gain: " + str(pre_adjust_r_gain))
+    print("G gain: " + str(pre_adjust_g_gain))
+    print("B gain: " + str(pre_adjust_b_gain))
+    print("Overall gain: " + str(pre_adjust_overall_gain))
+    print("exposure: " + str(pre_adjust_exposure))
+    print('------------------------------------')
+
+    set_l_mean = set_custom_exposure() #default thresholds
+
+
+    post_adjust_r_gain = sensor.__read_reg(0x02)
+    post_adjust_g_gain = sensor.__read_reg(0x03)
+    post_adjust_b_gain = sensor.__read_reg(0x01)
+    post_adjust_overall_gain = sensor.__read_reg(0x00)
+    post_adjust_exposure = (sensor.__read_reg(0x08) << 8) + sensor.__read_reg(0x10)
+
+    print("R gain: " + str(post_adjust_r_gain))
+    print("G gain: " + str(post_adjust_g_gain))
+    print("B gain: " + str(post_adjust_b_gain))
+    print("Overall gain: " + str(post_adjust_overall_gain))
+    print("exposure: " + str(post_adjust_exposure))
+    print()
+
+    print(i2c_obj)
+    print("Beginning...")
+    packed_data = ustruct.pack("<i", post_adjust_r_gain, post_adjust_g_gain, post_adjust_b_gain, post_adjust_overall_gain, post_adjust_exposure)
+
+    while(True):
+        try:
+            i2c_obj.send(packed_data,timeout=2000)
+
+        except OSError as err:
+            print(err)
+            pass # Don't care about errors - so pass.
+            # Note that there are 3 possible errors. A timeout error, a general purpose error, or
+            # a busy error. The error codes are 116, 5, 16 respectively for "err.arg[0]".
+
+
 
     img = sensor.snapshot()
 
@@ -114,7 +178,7 @@ if __name__ == "__main__":
 
     blob_index = -1
 
-    stage_one_bad_thresholds = [(20, 100, -4, 127, 2, 128)]
+    stage_one_bad_thresholds = [(20, 100, -10, 127, 3, 128)]
 
     for blob_index, stage_one_bad_blob in enumerate(img.find_blobs(stage_one_bad_thresholds, pixels_threshold=100, area_threshold=100, merge = False, margin = 15)):
         rect_stats = img.get_statistics(roi = stage_one_bad_blob.rect())
@@ -165,7 +229,7 @@ if __name__ == "__main__":
     ########### FIND GOOD BLOBS
 
     #stage_one_good_thresholds = [(img_stats.l_mean() - 1, 100, -127, img_stats.a_mean() - 4, img_stats.b_mean() - 8, 60)]
-    stage_one_good_thresholds = [(30, 100, -127, -3, -15, 3)]
+    stage_one_good_thresholds = [(25, 100, -127, -3, -15, 3)]
 
     for blob_index, stage_one_good_blob in enumerate(img.find_blobs(stage_one_good_thresholds, pixels_threshold=100, area_threshold=100, merge = False, margin = 15)):
         rect_stats = img.get_statistics(roi = stage_one_good_blob.rect())
@@ -228,6 +292,8 @@ if __name__ == "__main__":
     print(img.compress_for_ide(quality = 100))
 
     print("~~~~~~~~~~~~~~~ RESULTS: ~~~~~~~~~~~~~~~~")
+    print("good thresholds: " + str(stage_one_good_thresholds))
+    print("bad thresholds: " + str(stage_one_bad_thresholds))
     print("unhealthy full l mean: " + str(unhealthy_full_l_mean))
     print("unhealthy full a mean: " + str(unhealthy_full_a_mean))
     print("unhealthy full b mean: " + str(unhealthy_full_b_mean))
